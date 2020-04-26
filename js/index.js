96d9578e46de71983606057c963f5502ce547ebc
KYHyeon <run190822@gmail.com>
2020-04-26 23:44:45 +0900

    Update #2

--- js/index.js
+++ js/index.js
@@ -1,23 +1,53 @@
-$(document).ready(() => {
-    const canvas = $('#canvas')[0];
+function init(ctx) {
+    // 테두리 그리기
+    ctx.beginPath();
+    ctx.rect(0, 0, 800, 800);
+    ctx.stroke();
+
+    let step = 10;
+    for (let i = 0; i < step; i++) {
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        ctx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+        ctx.fillRect(i / 10 * 800, 0, 800 / step, 800);
+    }
+}
+
+// 캔버스 위치로 부터 구역 반환
+function getSection(x, y) {
+    console.log('Section : ' + Math.floor(x / 80));
+    return Math.floor(x / 80);
+}
+
+function redraw(section) {
+    const $canvas = $('#canvas');
+    const canvas = $canvas[0];
     const ctx = canvas.getContext('2d');
-    // 무지개 만들기
-    //fillRect(x좌표, y좌표, 가로크기, 세로크기)
-    ctx.fillStyle = 'rgb(255,0,0)';
-    ctx.fillRect(0, 0, 50, 100);
-    ctx.fillStyle = 'rgb(255,100,0)';
-    ctx.fillRect(50, 0, 50, 100);
-    ctx.fillStyle = 'rgb(255,255,0)';
-    ctx.fillRect(100, 0, 50, 100);
-    ctx.fillStyle = 'rgb(0,255,0)';
-    ctx.fillRect(150, 0, 50, 100);
-    ctx.fillStyle = 'rgb(0,255,0)';
-    ctx.fillRect(150, 0, 50, 100);
-    ctx.fillStyle = 'rgb(0,0,255)';
-    ctx.fillRect(200, 0, 50, 100);
-    ctx.fillStyle = 'rgb(0,0,100)';
-    ctx.fillRect(250, 0, 50, 100);
-    ctx.fillStyle = 'rgb(255,0,255)';
-    ctx.fillRect(300, 0, 50, 100);
-    //아래 정의된 캔버스의 크기를 오버할 경우 짤린다.
+
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    ctx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+    ctx.fillRect(section * 80, 0, 800 / 10, 800);
+}
+
+//캔버스 내의 마우스 이벤트 핸들러
+function onMouseDown(x, y) {
+    console.log('Mouse Down {' + x + ', ' + y + '}');
+    redraw(getSection(x, y));
+}
+
+$(document).ready(() => {
+    const $canvas = $('#canvas');
+    const canvas = $canvas[0];
+    const ctx = canvas.getContext('2d');
+
+    $canvas.mousedown(function () {
+        var x = event.x - $canvas.offset().left;
+        var y = event.y - $canvas.offset().top;
+        onMouseDown(x, y);
+    });
+
+    init(ctx);
 });
